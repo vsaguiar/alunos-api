a0ded54add6ddac42c150c2722cfb64411b039d8
Vinicius Silva <vsaguiar@outlook.com>
2023-11-20 17:08:04 -0300

    retornando todos os alunos

---
+++ AlunosApi/Controllers/AlunosController.cs
@@ -0,0 +1,34 @@
+﻿using AlunosApi.Models;
+using AlunosApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AlunosController : ControllerBase
+{
+    private IAlunoService _alunoService;
+
+    public AlunosController(IAlunoService alunoService)
+    {
+        _alunoService = alunoService;
+    }
+
+
+
+    [HttpGet]
+    public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+    {
+        try
+        {
+            var alunos = await _alunoService.GetAlunos();
+            return Ok(alunos);
+        }
+        catch 
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos.");
+        }
+    }
+
+}
